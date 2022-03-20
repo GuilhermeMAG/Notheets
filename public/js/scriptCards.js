6234a1a972c2539c92d409002a16b6b7ba8e1f0c
GuilhermeMAG <g.casagrande@uni9.edu.br>
2022-03-20 20:37:39 -0300

    Create scriptCards.js

---
+++ public/js/scriptCards.js
@@ -0,0 +1,36 @@
+const purshaseEl = document.querySelector('#purshaseEl')
+
+// console.log(sessionStorage.id)
+
+const ID = sessionStorage.id
+
+document.getElementById("compra").innerHTML = ID;
+
+//Compras
+const getPurshaseFromBack = async() => {
+    const response = await fetch(`http://localhost:5000/compras/000${ID}`)
+    return response
+}
+
+const addPurshaseToDom = async() => {
+    const purshase = await getPurshaseFromBack();
+    const div = document.createElement('div')
+    div.className = 'purshase'
+    div.innerHTML = `
+            <h3>${purshase.ID_da_compra}</h3>
+            <ul>
+            <li><strong>Descrição: </strong>${purshase.Descricao}</li>
+            <li><strong>Data da Compra: </strong>${purshase.Data_da_compra}</li>
+            <li><strong>Data do Envio: </strong>${purshase.Data_do_envio}</li>
+            </ul>
+            <div class="tags"><strong>Data estimada de entrega: </strong>${purshase.Data_estimada_de_entrega}</div>
+            `
+    purshaseEl.appendChild(div)
+
+}
+addPurshaseToDom()
+
+//Refresh
+function Atualizar() {
+    window.location.reload();
+}
