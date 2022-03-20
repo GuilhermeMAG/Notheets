db1fdcad708bda9f8f4329941cb98a28761a1537
GuilhermeMAG <g.casagrande@uni9.edu.br>
2022-03-20 20:37:59 -0300

    Update compras_notion.js

--- src/services/compras_notion.js
+++ src/services/compras_notion.js
@@ -15,8 +15,8 @@ module.exports = async function getCompras() {
         // console.log(page.properties.Descricao.formula.string);
         return {
             id: page.id,
-            Descricao: page.properties.Descricao.formula.string,
             ID_da_compra: page.properties.ID_da_compra.title[0].text.content,
+            Descricao: page.properties.Descricao.formula.string,
             Data_da_compra: page.properties.Data_da_compra.date.start,
             Data_do_envio: page.properties.Data_do_envio.date.start,
             Data_estimada_de_entrega: page.properties.Data_estimada_de_entrega.formula.date.start,
