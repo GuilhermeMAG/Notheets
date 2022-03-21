fb4a88ef2845f52ca87ca9ed04515b0ec3683d7f
GuilhermeMAG <g.casagrande@uni9.edu.br>
2022-03-21 16:36:09 -0300

    Update scriptCards.js

--- public/js/scriptCards.js
+++ public/js/scriptCards.js
@@ -8,7 +8,7 @@ document.getElementById("compra").innerHTML = ID;
 
 //Compras
 const getPurshaseFromBack = async() => {
-    const response = await fetch(`https://notheets.herokuapp.com/compras/000${ID}`)
+    const response = await fetch(`http://localhost:5000/compras/000${ID}`)
     const data = await response.json()
     return data
 }
@@ -19,13 +19,40 @@ const addPurshaseToDom = async() => {
     const div = document.createElement('div')
     div.className = 'purshase'
     div.innerHTML = `
-            <h3>${purshase.ID_da_compra}</h3>
-            <ul>
-            <li><strong>Descrição: </strong>${purshase.Descricao}</li>
-            <li><strong>Data da Compra: </strong>${purshase.Data_da_compra}</li>
-            <li><strong>Data do Envio: </strong>${purshase.Data_do_envio}</li>
-            </ul>
-            <div class="tags"><strong>Data estimada de entrega: </strong>${purshase.Data_estimada_de_entrega}</div>
+                <div class="headerId">
+                <h1>${purshase.ID_da_compra}</h1>
+                </div>
+            <br>
+                <div class="inputBox">
+                <label for="_descricao">Descrição:</label>
+                <input type="text" name="descricao" id="_descricao" autocomplete="on" value="${purshase.Descricao}" required>
+                </div>
+            <br><br>
+                <div class="inputBox">
+                <label for="data_compra"><b>Data da Compra:</b></label>
+                <input type="date" name="compra" id="data_compra" value="${purshase.Data_da_compra}" required>
+                </div>
+            <br><br>
+                <div class="inputBox">
+                <label for="data_do_envio"><b>Data do envio:</b></label>
+                <input type="date" name="envio" id="data_do_envio" value="${purshase.Data_do_envio}" required>
+                </div>
+            <br><br>
+                <div class="inputBox">
+                <label for="data_estimada_de_entrega"><b>Data estimada de entrega:</b></label>
+                <input type="date" name="entrega" id="data_estimada_de_entrega" value="${purshase.Data_estimada_de_entrega}" required>
+                </div>
+            <br><br>
+            <div class="controls">
+                <button class="editCardButton" style="font-size:30px" onclick="Atualizar()">
+                    <i class="fa fa-undo"></i></button>
+                <button class="editCardButton" style="font-size:30px" onclick="document.location='../index.html'">
+                    <i class="fa fa-chevron-left"></i></button>
+                <button class="editCardButton" style="font-size:30px" onclick="document.location='../index.html'">
+                    <i class="fa fa-share-square-o"></i></button>
+                <button class="editCardButton" style="font-size:30px" onclick="document.location='../index.html'">
+                    <i class="fa fa-check-square-o"></i></button>
+            </div>
             `
     purshaseEl.appendChild(div)
 
